bd0e82c4cfa6225ef73430379751e2bac1b3988c
ETE12 <emilianotoes12@gmail.com>
2024-10-14 09:10:38 -0600

    update media

--- src/app/media/media.component.spec.ts
+++ src/app/media/media.component.spec.ts
@@ -21,22 +21,22 @@ describe('MediaComponent', () => {
   });
 
   it('should return media = 550.6 for the first input', (done) => {
-    const file = new File(["160,591, 114, 229, 230, 270, 128, 1657, 624, 1503\n15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2"], "numbers.txt", { type: 'text/plain' });
+    const file = new File(["160,591,114,229,230,270,128,1657,624,1503\n15.0,69.9,6.5,22.4,28.4,65.9,19.4,198.7,38.8,138.2"], "numbers.txt", { type: 'text/plain' });
     component.calculateMedia(file);
     
     setTimeout(() => {
-    expect(component.media1).toBe(550.6);
-    done();
+      expect(component.media1).toBe(550.6);
+      done();
     }, 100);
-    });
-    
-    it('should return media = 60.32 for the second input', (done) => {
-    const file = new File(["160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503\n15.0, 69.9, 6.5, 22. 4, 28.4, 65. 9, 19. 4, 198. 7, 38.8, 138.2"], "numbers.txt", { type: 'text/plain' });
+  });
+
+  it('should return media = 60.32 for the second input', (done) => {
+    const file = new File(["160,591,114,229,230,270,128,1657,624,1503\n15.0,69.9,6.5,22.4,28.4,65.9,19.4,198.7,38.8,138.2"], "numbers.txt", { type: 'text/plain' });
     component.calculateMedia(file);
     
     setTimeout(() => {
-    expect(component.media2).toBe(60.32);
-    done();
+      expect(component.media2).toBe(60.32);
+      done();
     }, 100);
-    });
+  });
 });
